001f7789389cb7e857ebcc5d01aa938d8ab14c33
Aditto Mahmood <adittomango465@gmail.com>
2021-07-15 14:23:15 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+let btnOpen = document.querySelectorAll('.show-modal');
+let btnClose = document.querySelector('.close-modal');
+let overlay = document.querySelector('.overlay');
+let modal = document.querySelector('.modal');
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+for (let i = 0; i < btnOpen.length; i++) {
+  btnOpen[i].addEventListener('click', openModal);
+}
+btnClose.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
